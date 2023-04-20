fe44ccefae086ccf51b14897541e8dde9f7a675b
mircozenBalozen <mircea.rautoiu@gmail.com>
2023-04-20 19:43:46 +0300

    lab4: Finish lab4

---
+++ lab4/Shop.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include "UI/FruitUI.h"
+
+using namespace std;
+
+int main() {
+    unique_ptr<FruitRepo> repoPtr = make_unique<FruitRepo>();
+    unique_ptr<FruitController> controllerPtr = make_unique<FruitController>(std::move(repoPtr));
+    FruitUI ui(std::move(controllerPtr));
+
+    ui.mainMenu();
+
+    system("pause");
+}
