33b3b26bdc5db6aa4b49020b82ce065080f0bff3
Mircox <mircea.rautoiu@gmail.com>
2023-04-28 14:29:25 +0300

    ...

--- lab4/Shop.cpp
+++ lab4/Shop.cpp
@@ -7,6 +7,8 @@ using namespace ui;
 int main() {
     unique_ptr<FruitRepo> repoPtr = make_unique<FruitRepo>();
     unique_ptr<FruitController> controllerPtr = make_unique<FruitController>(std::move(repoPtr));
+
+
     FruitUI ui(std::move(controllerPtr));
 
     ui.mainMenu();
